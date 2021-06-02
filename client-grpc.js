fbccb51ad4ce47e9dee3ec0ee4bf8aa80aeebb2a
frmauro <frmauro8@gmail.com>
2021-06-02 16:26:37 -0300

    change url connect

--- client-grpc.js
+++ client-grpc.js
@@ -1,7 +1,25 @@
-const grpc = require('grpc');
-const PROTO_PATH = './notes.proto';
-const NoteService = grpc.load(PROTO_PATH).NoteService;
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
 
-const client = new NoteService('localhost:50051', grpc.credentials.createInsecure());
+const PROTO_PATH = './notes.proto';
+
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,//
+     defaults: true,
+     oneofs: true
+    });
+
+var NoteService = grpc.loadPackageDefinition(packageDefinition).NoteService;
+
+//const client = new NoteService('127.0.0.1:50051', grpc.credentials.createInsecure());
+//const client = new NoteService('localhost:50051', grpc.credentials.createInsecure());
+//const client = new NoteService('host.docker.internal', grpc.credentials.createInsecure());
+const client = new NoteService('0.0.0.0:50051', grpc.credentials.createInsecure());
+
+//const client = new NoteService('localhost:50051', grpc.credentials.createSsl());
+//const client = new NoteService('127.0.0.1:50051', grpc.credentials.createSsl());
 
 module.exports = client;
